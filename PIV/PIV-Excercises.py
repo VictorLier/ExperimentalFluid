4c3f0b1ef93fed073213b00de1dd3192723e348f
VictorLier <victor@lier.dk>
2024-01-11 10:46:20 +0100

    PIV-Excercises

---
+++ PIV/PIV-Excercises.py
@@ -0,0 +1,10 @@
+#1 Magnification is 0.1 as the physical object is 10 times the size of the image on the sensor
+
+#2 particle is 10 muM therefor the image of the particale is 1 muM. With the pixel pitch of 5 mu M the image is 1%5 of the pixel.
+
+#3 Diffranction limited spo
+
+F = 1*10**(-5)/(2.44 * (1+0.1)*5.32*10**(-7))
+print(F)
+
+#F nummeret skal være F/8
