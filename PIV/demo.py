d874b0156123a28c181578d8f0055233c4ee42c0
VictorLier <victor@lier.dk>
2024-01-11 10:05:08 +0100

    Demo update

--- PIV/demo.py
+++ PIV/demo.py
@@ -3,25 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from par2vel.camera import One2One
 from par2vel.field import Field2D
-from par2vel.artimage import ArtImage, constUfunc, OseenUfunc
-from par2vel.piv2d import fftdx
+from par2vel.artimage import ArtImage, OseenUfunc
+from par2vel.piv2d import dxfield
 
 cam = One2One((512,512))
+cam.noise_mean = 0.05
+cam.noise_rms = 0.01
 ai = ArtImage(cam)
-ai.random_particles(0.02)
-dx =np.array([5.1,2.2])
-#ai.displace_particles(constUfunc(dx), 1)
+ai.random_particles(0.02)  # particle density in particles per pixel
 ai.displace_particles(OseenUfunc(200,8,[256,256,0.0]), 1)
 ai.generate_images()
-iagrid = Field2D(cam)
-iagrid.squarewindows(32, 0.5)
-fftdx(ai.Im[0], ai.Im[1], iagrid)
+fld = Field2D(cam)
+fld.squarewindows(32, 0.5)
+dxfield(ai.Im[0], ai.Im[1], fld)
 
 plt.figure(1)
+plt.clf()
 plt.imshow(ai.Im[0], cmap='gray')
 plt.figure(2)
+plt.clf()
 plt.imshow(ai.Im[1], cmap='gray')
 plt.figure(3)
-plt.quiver(iagrid.x[0,:,:],iagrid.x[1,:,:],iagrid.dx[0,:,:],iagrid.dx[1,:,:])
+plt.clf()
+plt.quiver(fld.x[0,:,:],fld.x[1,:,:],fld.dx[0,:,:],fld.dx[1,:,:], pivot='mid')
 plt.axis('image')
 plt.show()
